f8dc866ab34c16e516285f406dc036d407c8f9df
Meet Chaudhari <mdc8464@gmail.com>
2020-04-10 12:37:54 +0530

    unCommented the whitenoise thing

--- ngo/ngo/settings.py
+++ ngo/ngo/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     STATIC_DIR,
 ]
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
